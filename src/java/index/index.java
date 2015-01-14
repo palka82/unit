9458efe6242e22c52dcb90fe48304e6ceeddcb3b
Кот <@192.168.0.117>
2015-01-14 18:14:55 +0600

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml

--- src/java/index/index.java
+++ src/java/index/index.java
@@ -37,9 +37,12 @@ import settings.Sqlset;
 import support.DateAdapter;
 import support.JarScan;
 import support.StringAdapter;
+import support.db.Column;
 import support.db.Creator;
 import support.db.Dao;
 import support.db.Persistence;
+import support.db.TableComparator;
+import support.enums.DbTypes;
 import support.logic.RightObject;
 import support.web.AbsEnt;
 import support.web.HrefOptionInterface;
@@ -64,7 +67,7 @@ public class index extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        if(StringAdapter.isNull(request.getSession().getAttribute("key"))){
+        if (StringAdapter.isNull(request.getSession().getAttribute("key"))) {
             response.sendRedirect("/unit/auth");
         }
         response.setContentType("text/html;charset=UTF-8");
@@ -72,121 +75,124 @@ public class index extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
-            out.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"css/index.css\">\n" +
-"	<script src=\"js/SIPml-api.js?svn=222\" type=\"text/javascript\"> </script><script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-latest.min.js\"></script>");
+            out.println("<head><link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"css/index.css\">\n"
+                    + "	<script src=\"js/SIPml-api.js?svn=222\" type=\"text/javascript\"> </script><script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-latest.min.js\"></script>");
             out.println("<title>Servlet index</title>");
             out.println("</head>");
             out.println("<body>");
-            try{
-            Connection con = Connect.createConnection(new Sqlset());
+            try {
+                Connection con = Connect.createConnection(new Sqlset());
                 try {
-                    Creator.saveNewtables(con,index.class);
-                    WebClientImpl wc= new WebClientImpl(request,new Project(),new Sqlset());
-                    Map<String,Object> requestParam=wc.getInnerRequest();
-                    String result="";
-                    result+=getHtmlMenu();
-                    result+="<div style='float:left;width:100%;margin-top:50px;'>";
-                    if(StringAdapter.NotNull(requestParam.get("logout"))){
+                    WebClientImpl wc = new WebClientImpl(request, new Project(), new Sqlset());
+                    Map<String, Object> requestParam = wc.getInnerRequest();
+                    String result = "";
+                    result += getHtmlMenu();
+                    result += "<div style='float:left;width:100%;margin-top:50px;'>";
+                    if (StringAdapter.NotNull(requestParam.get("logout"))) {
                         request.getSession().removeAttribute("key");
                         request.getSession().invalidate();
                         response.sendRedirect(request.getContextPath());
                     }
-                    Dao dao=Dao.getInstance(con);
+                    Dao dao = Dao.getInstance(con);
+                    List<String> log = new ArrayList();
+                    sysTableToSql(log, con);
                     
-                    Persistence persistence=Persistence.getInstance();
-                    RightObject listRights=persistence.createRightsFromJar(index.class);
-                    for(support.logic.Right jarRight:listRights.getRights()){
-                        entity.Rights right=new entity.Rights();
-                        right.object=jarRight.getObject();
-                        right.action=jarRight.getAction();
-                        if(dao.findByValues(right).isEmpty()){
+                    Persistence persistence = Persistence.getInstance();
+                    RightObject listRights = persistence.createRightsFromJar(index.class);
+                    for (support.logic.Right jarRight : listRights.getRights()) {
+                        entity.Rights right = new entity.Rights();
+                        right.object = jarRight.getObject();
+                        right.action = jarRight.getAction();
+                        if (dao.findByValues(right).isEmpty()) {
                             out.println(right.rightId);
-                            right.addDate=new Date();
+                            right.addDate = new Date();
                             dao.save(right);
-                            right.addDate=null;
+                            right.addDate = null;
                         }
                     }
-                    
-                    ControllerAbstract controller=null;
-                    
-                    
-                    RightObject userRight=listRights;
-                    
-                    if(StringAdapter.NotNull(wc.getObject(),wc.getAction())){
-                        String realName="controller."+StringAdapter.ucFirst(wc.getObject());
-                        String realMethod=wc.getAction();
-                    
-                        if(userRight.isRight(realName, realMethod)){
-  
-                            Class contr=Class.forName(realName);
+
+                    ControllerAbstract controller = null;
+
+                    RightObject userRight = listRights;
+
+                    if (StringAdapter.NotNull(wc.getObject(), wc.getAction())) {
+                        String realName = "controller." + StringAdapter.ucFirst(wc.getObject());
+                        String realMethod = wc.getAction();
+
+                        if (userRight.isRight(realName, realMethod)) {
+
+                            Class contr = Class.forName(realName);
                             Object obj = contr.newInstance();
-                            controller=(ControllerAbstract) obj;
+                            controller = (ControllerAbstract) obj;
                             controller.setDao(dao);
                             controller.setRequest(requestParam);
                             controller.setSession(wc.getInnerSession());
 
-                            Method method = contr.getMethod(realMethod); 
-                            method.invoke(obj); 
+                            Method method = contr.getMethod(realMethod);
+                            method.invoke(obj);
 
-                         }
+                        }
                     }
-                    if(controller!=null){
-                        result+=controller.getResult()+"</div>";
-                    }else{
-                        result+="</div>";
+                    if (controller != null) {
+                        result += controller.getResult() + "</div>";
+                    } else {
+                        result += "</div>";
                     }
-                
+
                     out.println(result);
+                    for (String logs : log) {
+                        out.println(logs + "</br>");
+                    }
                     out.println(request.getSession().getAttribute("name"));
-                    if(con!=null){
-                        if(!con.isClosed()){
-                           con.close();
+                    if (con != null) {
+                        if (!con.isClosed()) {
+                            con.close();
                         }
                     }
                 } catch (Exception ex) {
                     out.println(StringAdapter.getStackTraceException(ex));
-                    if(con!=null){
-                        if(!con.isClosed()){
-                           con.close();
+                    if (con != null) {
+                        if (!con.isClosed()) {
+                            con.close();
                         }
                     }
                 }
-            }catch(Exception e){
+            } catch (Exception e) {
                 out.println(StringAdapter.getStackTraceException(e));
             }
-            
+
             out.println("</body>");
             out.println("</html>");
         }
 
     }
-    
-    private String getHtmlMenu() throws Exception{
+
+    private String getHtmlMenu() throws Exception {
         FabricRender fr = FabricRender.getInstance(new Project());
 
-        AbsEnt base=fr.div("float:left;width:100%", null);
-            
-        AbsEnt div= fr.div("float:left;width:100%", null);
+        AbsEnt base = fr.div("float:left;width:100%", null);
+
+        AbsEnt div = fr.div("float:left;width:100%", null);
         base.addEnt(div);
-        
+
         List<Parameter> list = new ArrayList();
-        HrefOptionInterface demo=fr.getHrefOption();
+        HrefOptionInterface demo = fr.getHrefOption();
         demo.setObject("demo");
         demo.setAction("setUserDemo");
         demo.setName("Демо");
         demo.setNoValidateRights();
-        
+
         div.addEnt(fr.href(list, demo).setCss("knopka"));
-        
-        HrefOptionInterface users=fr.getHrefOption();
+
+        HrefOptionInterface users = fr.getHrefOption();
         users.setObject("user");
         users.setAction("showUsers");
         users.setNoValidateRights();
         users.setName("Пользователи");
         div.addEnt(fr.href(list, users).setCss("knopka"));
-        
+
         List<Parameter> list2 = new ArrayList();
-        HrefOptionInterface roles=fr.getHrefOption();
+        HrefOptionInterface roles = fr.getHrefOption();
         roles.setObject("role");
         roles.setAction("showRoles");
         roles.setNoValidateRights();
@@ -194,15 +200,15 @@ public class index extends HttpServlet {
         div.addEnt(fr.href(list2, roles).setCss("knopka"));
 
         List<Parameter> list3 = new ArrayList();
-        HrefOptionInterface treatments=fr.getHrefOption();
+        HrefOptionInterface treatments = fr.getHrefOption();
         treatments.setObject("Treatment");
         treatments.setAction("showTreatments");
         treatments.setNoValidateRights();
         treatments.setName("Причины обращения");
         div.addEnt(fr.href(list3, treatments).setCss("knopka"));
-      
+
         List<Parameter> list4 = new ArrayList();
-        HrefOptionInterface patientstypes=fr.getHrefOption();
+        HrefOptionInterface patientstypes = fr.getHrefOption();
         patientstypes.setObject("PatientsTypes");
         patientstypes.setAction("showPatientsTypes");
         patientstypes.setNoValidateRights();
@@ -210,7 +216,7 @@ public class index extends HttpServlet {
         div.addEnt(fr.href(list4, patientstypes).setCss("knopka"));
 
         List<Parameter> list5 = new ArrayList();
-        HrefOptionInterface talkresult=fr.getHrefOption();
+        HrefOptionInterface talkresult = fr.getHrefOption();
         talkresult.setObject("TalkResult");
         talkresult.setAction("showTalkResult");
         talkresult.setNoValidateRights();
@@ -218,7 +224,7 @@ public class index extends HttpServlet {
         div.addEnt(fr.href(list5, talkresult).setCss("knopka"));
 
         List<Parameter> list6 = new ArrayList();
-        HrefOptionInterface doctors=fr.getHrefOption();
+        HrefOptionInterface doctors = fr.getHrefOption();
         doctors.setObject("Doctors");
         doctors.setAction("showDoctors");
         doctors.setNoValidateRights();
@@ -227,17 +233,49 @@ public class index extends HttpServlet {
 
         List<Parameter> endlist = new ArrayList();
         list2.add(new Parameter("logout", "logout"));
-        HrefOptionInterface logout=fr.getHrefOption();
+        HrefOptionInterface logout = fr.getHrefOption();
         logout.setNoValidateRights();
         logout.setName("Выход");
         div.addEnt(fr.href(list2, logout).setCss("knopka"));
-        
-        
-
 
         return base.render();
     }
-    
+
+    private void sysTableToSql(List<String> log, Connection con) throws Exception {
+        log.add("<b>Получение системных таблиц</b>");
+        List<support.db.Table> sysTable = Persistence.getInstance().createTableFromJar(index.class);
+        log.addAll(showTableStructure(sysTable));
+        log.add("<b>Получение Mysql таблиц</b>");
+        List<support.db.Table> sqltabl = Creator.getTablesFromSql(con);
+        log.addAll(showTableStructure(sqltabl));
+        log.add("<b>Сравнение таблиц</b>");
+        TableComparator tc = TableComparator.getInstance(sysTable, sqltabl);
+        tc.compare();
+        log.add("<b>Удаление таблиц</b>");
+        log.addAll(showTableStructure(tc.getTableToDrop()));
+        Creator.createTables(con, DbTypes.MySQL, tc.getTableToDrop());
+        log.add("<b>Добавление таблиц</b>");
+        log.addAll(showTableStructure(tc.getTableToCreate()));
+        Creator.createTables(con, DbTypes.MySQL, tc.getTableToCreate());
+        log.add("<b>Удаление столбцов таблиц</b>");
+        log.addAll(showTableStructure(tc.getTableToDropColumn()));
+        Creator.dropColumnInTables(con, DbTypes.MySQL, tc.getTableToCreate());
+        log.add("<b>Добавление столбцов таблиц</b>");
+        log.addAll(showTableStructure(tc.getTableAddColumn()));
+        Creator.addColumnInTables(con, DbTypes.MySQL, tc.getTableToCreate());
+    }
+
+    private List<String> showTableStructure(List<support.db.Table> request) throws Exception {
+        List<String> result = new ArrayList();
+        for (support.db.Table sqT : request) {
+            result.add("TableName <b>" + sqT.name + "</b>");
+            List<Column> li = sqT.getColumns();
+            for (Column cln : li) {
+                result.add("name: " + cln.name + ", type: " + cln.type + ", primary: " + cln.isPrimary + ", null: " + cln.isNull);
+            }
+        }
+        return result;
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
